460e954c1f52e2502630db32a9b2da2c57137746
natacioTecnologia <dev.nataciotecnologia.com.br>
2023-06-09 18:32:05 -0300

    primeiro evento

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './style.css';
 
 function Home() {
+  const [counter, setCounter] = useState(0);
+
+  const handleCOunter = ()  => {
+    setCounter(counter + 1)
+  }
   return (
     <div className="Home">
       <div>Ola</div>
+      <button onClick={handleCOunter}>Counter : {counter}</button>
     </div>
   );
 }

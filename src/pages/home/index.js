ba1466fe3ce064075a506290e609a3ecd37a0c23
natacioTecnologia <dev.nataciotecnologia.com.br>
2023-06-09 21:35:39 -0300

    otimizaçao de componente usando memo e hook

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import './style.css';
+import { useCallback , memo} from 'react';
+
+const MyButton = memo( function ({handleCOunter}){
+  console.log("Renderizou")
+  return(
+    <div className='MyButton'>
+      <button onClick={handleCOunter}>Counter </button>
+
+    </div>
+  )
+})
 
 function Home() {
   const [counter, setCounter] = useState(0);
 
-  const handleCOunter = ()  => {
-    setCounter(counter + 1)
-  }
+  const handleCOunter = useCallback(()  => {
+    setCounter((c)=> c + 1)
+  }, [])
   return (
     <div className="Home">
-      <div>Ola</div>
-      <button onClick={handleCOunter}>Counter : {counter}</button>
+      <div>{counter}</div>
+      <MyButton handleCOunter={handleCOunter} />
     </div>
   );
 }
